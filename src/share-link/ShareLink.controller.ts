cae91f18906349bd44e087136ded0233f9597d04
keizir <ultimatesofteng.work@gmail.com>
2023-01-25 08:34:34 -0800

    fix: facebook meta tag

--- src/share-link/ShareLink.controller.ts
+++ src/share-link/ShareLink.controller.ts
@@ -37,8 +37,7 @@ export class ShareLinkController {
             <meta name="title" content=${title} />
             <meta name="description" content=${description} />
 
-            <meta property="og:type" content="article" />
-            <meta property="og:url" content=${url} />
+            <meta property="og:type" content="website" />
             <meta property="og:title" content=${title} />
             <meta property="og:description" content=${description} />
             <meta property="og:image" content=${image} />
